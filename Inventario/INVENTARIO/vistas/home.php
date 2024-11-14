d561efa9d941a7812d17c63d41e31b9fc469bca0
OhmsSky <168183669+OhmsSky@users.noreply.github.com>
2024-11-14 00:29:51 -0300

    Commit Inicial

---
+++ Inventario/INVENTARIO/vistas/home.php
@@ -0,0 +1,5 @@
+<div class="container is-fluid center2 notification is-info is-dark">
+	<h1 class="title">Inicio</h1>
+	<h2 class="subtitle">¡Bienvenid@ <?php echo $_SESSION['nombre']." ".$_SESSION['apellido']; ?>!</h2>
+
+</div>
