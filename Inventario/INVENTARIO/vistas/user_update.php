09c399c5476bbe39d11d146be8ac8a54e58fe2f9
Mitchel Bustamante <mitchel.bustamante0123@gmail.com>
2024-11-15 23:19:05 -0300

    User Update modificación de input de texto (Se me olvidó modificarlo kkkkk)

--- Inventario/INVENTARIO/vistas/user_update.php
+++ Inventario/INVENTARIO/vistas/user_update.php
@@ -38,13 +38,13 @@
 				<div class="column">
 					<div class="control">
 						<label>Nombres</label>
-						<input class="input" type="text" name="usuario_nombre" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,40}" maxlength="40" required value="<?php echo $datos['usuario_nombre']; ?>" >
+						<input class="input is-rounded is-danger is-medium" type="text" name="usuario_nombre" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,40}" maxlength="40" required value="<?php echo $datos['usuario_nombre']; ?>" >
 					</div>
 				</div>
 				<div class="column">
 					<div class="control">
 						<label>Apellidos</label>
-						<input class="input" type="text" name="usuario_apellido" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,40}" maxlength="40" required value="<?php echo $datos['usuario_apellido']; ?>" >
+						<input class="input is-rounded is-danger is-medium" type="text" name="usuario_apellido" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,40}" maxlength="40" required value="<?php echo $datos['usuario_apellido']; ?>" >
 					</div>
 				</div>
 			</div>
@@ -52,13 +52,13 @@
 				<div class="column">
 					<div class="control">
 						<label>Usuario</label>
-						<input class="input" type="text" name="usuario_usuario" pattern="[a-zA-Z0-9]{4,20}" maxlength="20" required value="<?php echo $datos['usuario_usuario']; ?>" >
+						<input class="input is-rounded is-danger is-medium" type="text" name="usuario_usuario" pattern="[a-zA-Z0-9]{4,20}" maxlength="20" required value="<?php echo $datos['usuario_usuario']; ?>" >
 					</div>
 				</div>
 				<div class="column">
 					<div class="control">
 						<label>Email</label>
-						<input class="input" type="email" name="usuario_email" maxlength="70" value="<?php echo $datos['usuario_email']; ?>" >
+						<input class="input is-rounded is-danger is-medium" type="email" name="usuario_email" maxlength="70" value="<?php echo $datos['usuario_email']; ?>" >
 					</div>
 				</div>
 			</div>
@@ -71,13 +71,13 @@
 				<div class="column">
 					<div class="control">
 						<label>Clave</label>
-						<input class="input" type="password" name="usuario_clave_1" pattern="[a-zA-Z0-9$@.-]{7,100}" maxlength="100" >
+						<input class="input is-rounded is-danger is-medium" type="password" name="usuario_clave_1" pattern="[a-zA-Z0-9$@.-]{7,100}" maxlength="100" >
 					</div>
 				</div>
 				<div class="column">
 					<div class="control">
 						<label>Repetir clave</label>
-						<input class="input" type="password" name="usuario_clave_2" pattern="[a-zA-Z0-9$@.-]{7,100}" maxlength="100" >
+						<input class="input is-rounded is-danger is-medium" type="password" name="usuario_clave_2" pattern="[a-zA-Z0-9$@.-]{7,100}" maxlength="100" >
 					</div>
 				</div>
 			</div>
@@ -89,18 +89,18 @@
 				<div class="column">
 					<div class="control">
 						<label>Usuario</label>
-						<input class="input" type="text" name="administrador_usuario" pattern="[a-zA-Z0-9]{4,20}" maxlength="20" required >
+						<input class="input is-rounded is-danger is-medium" type="text" name="administrador_usuario" pattern="[a-zA-Z0-9]{4,20}" maxlength="20" required >
 					</div>
 				</div>
 				<div class="column">
 					<div class="control">
 						<label>Clave</label>
-						<input class="input" type="password" name="administrador_clave" pattern="[a-zA-Z0-9$@.-]{7,100}" maxlength="100" required >
+						<input class="input is-rounded is-danger is-medium" type="password" name="administrador_clave" pattern="[a-zA-Z0-9$@.-]{7,100}" maxlength="100" required >
 					</div>
 				</div>
 			</div>
 			<p class="has-text-centered">
-				<button type="submit" class="button is-rounded is-danger animado">Actualizar</button>
+				<button type="submit" class="button is-rounded is-danger animado is-medium">Actualizar</button>
 			</p>
 		</form>
 		<?php 
