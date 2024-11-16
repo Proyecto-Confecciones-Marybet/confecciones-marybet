707ba3d233b98c5b5abc15d9e3400aa40ed5a428
OhmsSky <matiasramirezriveros@gmail.com>
2024-11-16 00:10:06 -0300

    Pequeña Modificación

--- Inventario/INVENTARIO/vistas/ayuda.php
+++ Inventario/INVENTARIO/vistas/ayuda.php
@@ -1,19 +1,29 @@
-<section class="section center">
-    <div class="center is-danger">
+<section class="section container2 center3">
+    <div class="container2 center notification is-danger">
         <h1 class="title">Ayuda</h1>
+    </div>
+</section>
+
+<section class="section container2 center">
+    <div class="container center3 notification is-danger">
         <p class="subtitle">Acá encontrarás soporte para cualquier problema o duda que tengas sobre el uso y funcionamiento de la aplicación</p>
     </div>
 </section>
 
-<section class="section container2 center3">
-    <div class="container center notification is-danger">
+<section class="section container2 center">
+    <div class="container center2 notification is-danger">
         <h2 class="title">Cómo Usar la Aplicación</h2>
         <p class="subtitle">Acá encontrarás ayuda y soluciones más comunes sobre el funcionamiento de la aplicación, qué puedes hacer y qué puedes esperar.</p>
     </div>
 </section>
 
-<section class="section container2 center">
-    <div class="container center notification is-danger">
+<section class="section center">
+    <div class="center is-danger">
+    </div>
+</section>
+
+<section class="section container center">
+    <div class="container center2 notification is-danger">
         <h2 class="title">Soporte</h2>
         <p class="subtitle">¿Tienes algún problema con la plataforma? Acá podrás encontrar ayuda para problemas más complejos.</p>
     </div>
